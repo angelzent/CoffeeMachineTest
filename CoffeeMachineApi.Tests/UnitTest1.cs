2a24a34e1df13795003d076f67eb47f8add90031
Vincent <angelzent03@gmail.com>
2026-04-09 05:07:47 +0800

    Add project files.

---
+++ CoffeeMachineApi.Tests/UnitTest1.cs
@@ -0,0 +1,57 @@
+using CoffeeMachineApi.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeMachineApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Should_Return_503_On_Every_5th_Call()
+        {
+            var controller = new CoffeeMachineController();
+
+            IActionResult? result = null;
+
+            for (int i = 0; i < 5; i++)
+            {
+                result = await controller.BrewCoffee();
+            }
+
+            var status = result as StatusCodeResult;
+
+            status!.StatusCode.Should().Be(503);
+        }
+
+        [Fact]
+        public async Task Should_Return_418_On_April_1()
+        {
+            var controller = new CoffeeMachineController();
+            controller.Clock = () => new DateTimeOffset(2026, 4, 1, 10, 0, 0, TimeSpan.Zero);
+
+            var result = await controller.BrewCoffee();
+
+            var status = result as StatusCodeResult;
+
+            status!.StatusCode.Should().Be(418);
+        }
+
+        [Fact]
+        public async Task Should_Return_Hot_Coffee()
+        {
+            // var weather = new FakeWeatherService(25);
+            var controller = new CoffeeMachineController();
+
+            var result = await controller.BrewCoffee();
+
+            var ok = result as OkObjectResult;
+
+            ok.Should().NotBeNull();
+
+            ok!.Value.ToString()
+                .Should()
+                .Contain("Your piping hot coffee is ready");
+        }
+
+    }
+}
---
+++ CoffeeMachineApi/Controllers/CoffeMachineController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace CoffeeMachineApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class CoffeeMachineController : ControllerBase
+    {
+        private static int _counter = 0;
+        private static readonly object _lock = new();
+        public Func<DateTimeOffset> Clock { get; set; } = () => DateTimeOffset.Now;
+
+        [HttpGet("brew-coffee")]
+        public async Task<IActionResult> BrewCoffee()
+        {
+            // Rule #3 — April 1st returns 418
+            var now = Clock();
+            if (now.Month == 4 && now.Day == 1)
+            {
+                return StatusCode(418); // I'm a teapot
+            }
+
+            int currentCount;
+
+            lock (_lock)
+            {
+                _counter++;
+                currentCount = _counter;
+            }
+
+            // Rule #2 — every 5th call returns 503
+            if (currentCount % 5 == 0)
+            {
+                return StatusCode(503);
+            }
+
+            // Rule #1 — normal response
+            var message = "Your piping hot coffee is ready"; 
+            var response = new
+            {
+                message,
+                prepared = now.ToString("yyyy-MM-ddTHH:mm:sszzz", CultureInfo.InvariantCulture)
+            };
+
+            return Ok(response);
+        }
+
+    }
+}
